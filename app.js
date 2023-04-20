d2c67a18745687acb7dee201ea6464e8dad33c7a
maxwu1104 <maxwu1104@gmail.com>
2023-04-20 20:59:49 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -2,8 +2,16 @@
 const express = require('express')
 const app = express()
 
+// require middleware
+const { engine } = require('express-handlebars')
+
+// view engine setting
+app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
+app.set('views', './views')
+
 // test server
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.render('index'))
 
 // start sever and listen
 app.listen(3000, () =>
