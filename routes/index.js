67afce6bef761bc9c512af87955a1343a5aac1ad
maxwu1104 <maxwu1104@gmail.com>
2023-04-26 23:20:35 +0800

    refactor: modify routes & function
    index.js
    1. 刪除 brand 相關的導入
    
    brand.js
    1. 刪除 brand.js ，將函式導入至 routes - home 直接使用
    
     home.js
    1. 新增如果輸入包含 hostname 的網址，則返回顯示 " 該網址不能縮短 "
    2. 新增原為 brand.js 內的函式
    3. 更新資料查找方式

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
-const brand = require('./modules/brand')
 
 router.use('/', home)
-router.use('/brand', brand)
 
 module.exports = router
--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,26 +1,47 @@
 const express = require('express')
 const router = express.Router()
 const Url = require('../../models/shortenUrl')
-const getFiveRandomWords = require('../../models/getFiveRandomWords')
+const getRandomWords = require('../../utils/getRandomWords')
 
 router.get('/', (req, res) => res.render('index'))
 
+router.get('/:shortUrl', (req, res) => {
+  const target = req.params.shortUrl
+  Url.findOne({ shortUrl: { $regex: `${target}` } })
+    .lean()
+    .exec()
+    .then((url) => {
+      res.redirect(url.originUrl)
+    })
+})
+
 router.post('/', (req, res) => {
-  const inputUrl = req.body
-  Url.find({ $or: [{ url: inputUrl.url }, { newUrl: inputUrl.url }] })
+  if (req.body.url.includes(req.hostname)) {
+    return res.render('index', {
+      message: '該網址不能縮短！',
+      url: req.body.url
+    })
+  }
+  if (req.body.url.length === 0) {
+    return res.render('index', { message: '請輸入網址' })
+  }
+
+  Url.findOne({ originUrl: req.body.url })
     .lean()
     .exec()
     .then(async (url) => {
-      if (url.length === 1) {
-        return res.render('index', { existingUrl: url[0].newUrl })
+      if (url) {
+        return res.render('index', { existingUrl: url.shortUrl })
       } else {
-        const value = await getFiveRandomWords()
+        const value = await getRandomWords(5)
+        const port = process.env.NODE_ENV === 'production' ? '' : ':3000'
         const url = new Url({
-          url: inputUrl.url,
-          newUrl: `http://localhost:3000/brand/${value}`
+          originUrl: req.body.url,
+          shortUrl:
+            req.protocol + '://' + req.hostname + `${port}` + '/' + `${value}`
         })
         url.save().then(() => {
-          return res.render('index', { newUrl: url.newUrl })
+          return res.render('index', { newUrl: url.shortUrl })
         })
       }
     })
